1ab95989a4653d957f98dc1e1f59883fef136377
Ilya Mukasei <ilya.mukosey@gmail.com>
2025-03-12 00:30:31 +0100

    created responsive header with all elements and sidebar ~3h

---
+++ src/components/Header.tsx
@@ -0,0 +1,73 @@
+import { Link } from 'react-router';
+import '../styles/header.scss';
+import { LogoIcon } from './icons/LogoIcon';
+import { JSX, useEffect, useRef, useState } from 'react';
+import { BurgerIcon } from './icons/BurgerIcon.tsx';
+import { CrossIcon } from './icons/CrossIcon.tsx';
+
+interface HeaderItems {
+  id: string;
+  title: string;
+  url: string;
+  component?: () => JSX.Element;
+}
+function Header({ items }: Readonly<{ items: HeaderItems[] }>) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const burgerRef = useRef<HTMLButtonElement>(null);
+
+  const handleResize = () => {
+    if (burgerRef.current && !burgerRef.current.offsetParent) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  const handleBurgerClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <header className="container header">
+      <Link to="/">
+        <LogoIcon />
+      </Link>
+      <div className="header__navigation">
+        {items.map((item) => {
+          if (item.component) {
+            return (
+              <Link to={item.url} key={item.id} className="header__link">
+                <item.component key={item.id} />
+              </Link>
+            );
+          } else {
+            return (
+              <Link to={item.url} key={item.id} className="header__link">
+                {item.title}
+              </Link>
+            );
+          }
+        })}
+      </div>
+      <button ref={burgerRef} className="burger-icon" onClick={handleBurgerClick}>
+        {isMenuOpen ? <CrossIcon /> : <BurgerIcon />}
+      </button>
+
+      <aside className={`sidebar ${isMenuOpen ? 'sidebar__open' : ''}`}>
+        <div className="sidebar__content">
+          {items.map((item) => (
+            <Link to={item.url} key={item.id} className="sidebar__link">
+              {item.title}
+            </Link>
+          ))}
+        </div>
+      </aside>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/icons/CrossIcon.tsx
@@ -0,0 +1,9 @@
+export const CrossIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 33 38">
+      <g stroke="#fff" strokeWidth="2">
+        <path d="m5.406 27.196 23-17M28.429 26.821l-23-16" />
+      </g>
+    </svg>
+  );
+};
